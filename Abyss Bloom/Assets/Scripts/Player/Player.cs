d3941826972fce9770c14e541d2de5e37ad4c163
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-08-17 16:59:30 +0530

    Player Variable Jump with Cinemachine camera

--- Abyss Bloom/Assets/Scripts/Player/Player.cs
+++ Abyss Bloom/Assets/Scripts/Player/Player.cs
@@ -6,6 +6,21 @@ public class Player : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
 
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+
+    [Header("Gravity Settings")]
+    [SerializeField] private float normalGravity;
+    [SerializeField] private float fallGravity;
+    [SerializeField] private float jumpGravity;
+
+    [Header("GroundCheck Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private PlayerInput playerInput;
@@ -15,6 +30,12 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Move();
+        ApplyGravity();
+    }
+
+    void Move()
+    {
         float targetSpeed = moveInput.x * speed;
         rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
     }
@@ -22,5 +43,41 @@ public class Player : MonoBehaviour
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && CheckGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        else
+        {
+            if(rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            }
+        }
+    }
+
+    void ApplyGravity()
+    {
+        if(rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else if(rb.linearVelocity.y > 0)
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
+
+    public bool CheckGrounded()
+    {
+        return isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 }
